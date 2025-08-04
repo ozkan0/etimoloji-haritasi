a6828f06a1c73313e0910fc65e959458b15df9df
Omer <oozkan23@outlook.com>
2025-08-04 10:17:39 +0300

    delete unused data files

--- types.ts
+++ types.ts
@@ -1,6 +1,3 @@
-// types.ts
-
-// Kelime verisinin temel yapısı
 export interface Word {
   id: number;
   word: string;
@@ -11,14 +8,12 @@ export interface Word {
   references?: string;
 }
 
-// Dil ve harita verisinin yapısı
 export interface Language {
   language: string;
   representativeCountry: string;
   boundingBox: [number, number, number, number];
 }
 
-// Haritada gösterilecek bir kelimenin, kendi verisine ek olarak koordinatlarını da içeren yapısı
 export interface WordOnMap extends Word {
   coordinates: [number, number];
 }
